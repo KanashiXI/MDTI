e4f1469901260e0f20eb44ee36e7c1fb21ea7bff
mekpongsathon <Pongsathon.Rakthong@gmail.com>
2020-03-01 16:48:01 +0700

    show epm data

--- view/index.php
+++ view/index.php
@@ -244,9 +244,9 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
       integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
@@ -268,13 +268,13 @@
       });
     }
       function loadData() {
-        var url = "http://localhost/MDTI/index.php/";
+        var url = "http://localhost/MDTI-master/index.php/jsonemp";
         $.getJSON(url).done(function (data) {
           console.log(JSON.stringify(data));
           var line = "";
           $.each(data, function (k, item) {
             console.log(item);
-            line += "<tr><td align='center'><img src='" + item.license + "' width='200'></td>";
+            line += "<tr><td align='center'>" + item.license + "</td>";
             line += "<td align='center'>" + item.name + "</td>";
             line += "<td align='center'>" + item.position + "</td>";
             line += "</tr>";
