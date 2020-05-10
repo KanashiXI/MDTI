ebaf067a94f79b2ee1d14ce60cbee5d8b9847c5f
KanashiXI <panyapat.test@gmail.com>
2020-05-10 22:01:40 +0700

    show all data on table

--- view/index.php
+++ view/index.php
@@ -17,103 +17,16 @@
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <!-- jquery -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-
+  <!-- custom css -->
+  <link rel="stylesheet" type="text/css" href="../custom.css">
   <title>index!</title>
 
-  <style>
-    /* search bar */
-    body,
-    html {
-      height: 100%;
-      width: 100%;
-      margin: 0;
-      padding: 0;
-      background: #72f1d1 !important;
-    }
-
-    .searchbar {
-      margin-bottom: 20px;
-      margin-top: 20px;
-      height: 60px;
-      background-color: #353b48;
-      border-radius: 30px;
-      padding: 10px;
-    }
-
-    .search_input {
-      color: white;
-      border: 0;
-      outline: 0;
-      background: none;
-      width: 0;
-      caret-color: transparent;
-      line-height: 40px;
-      transition: width 0.4s linear;
-    }
-
-    .searchbar:hover>.search_input {
-      padding: 0 10px;
-      width: 450px;
-      caret-color: red;
-      transition: width 0.4s linear;
-    }
-
-    .searchbar:hover>.search_icon {
-      background: white;
-      color: #e74c3c;
-    }
-
-    .search_icon {
-      height: 40px;
-      width: 40px;
-      float: right;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border-radius: 50%;
-      color: white;
-    }
-
-    /* end search bar */
-
-    img {
-      display: block;
-
-      margin-left: auto;
-      margin-right: auto;
-    }
-
-    p .card {
-      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-      transition: 0.3s;
-      width: 40%;
-    }
-
-    .card:hover {
-      box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-    }
-
-    .card-horizontal {
-      display: flex;
-      flex: 1 1 auto;
-    }
-
-    /* .container {
-      padding: 2px 16px;
-    } */
-
-    #con-card {
-      margin: 0px 0px 0px 70px;
-      display: inline-block;
-      position: relative;
-      width: 90%;
-      background-size: contain;
-      text-align: center;
-    }
-  </style>
+  
 </head>
 
 <body>
+ 
+
   <!-- nav bar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">TIme table</a>
@@ -121,7 +34,6 @@
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
@@ -143,7 +55,7 @@
           <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
         </div>
       </div>
-    </div>
+    
 
 
     <!-- end search -->
@@ -162,53 +74,9 @@
           <tbody id="rdall">
           </tbody>
         </table>
-        <div class="row">
-          <div class="col-md-3">
-              <div class="card">
-                  <div class="card-horizontal">
-                      <div class="card-body">
-                          <h4 class="card-title">Card title</h4>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="col-md-3">
-              <div class="card">
-                  <div class="card-horizontal">
-                      <div class="card-body">
-                          <h4 class="card-title">Card title</h4>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="col-md-3">
-              <div class="card">
-                  <div class="card-horizontal">
-                      <div class="card-body">
-                          <h4 class="card-title">Card title</h4>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="col-md-3">
-              <div class="card">
-                  <div class="card-horizontal">
-                      <div class="card-body">
-                          <h4 class="card-title">Card title</h4>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      </div>
-                  </div>
-              </div>
-          </div>
-        </div>
-      </div>
       <!-- ///////////////////////////////////////////// -->
     </div>
-    </div>
-
+  </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"
@@ -220,7 +88,6 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
       integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
-
     <script>
       function renderTable(data) {
         var rdall = $("#rdall");
@@ -252,62 +119,15 @@
             line += "<td align='center'>" + item.position + "</td>";
             line += "<td align='center'>" + item.section + "</td>";
             line += "<td align='center'>" + item.round + "</td>";
-            // line += "<td align='center'>" + item.condition[0] + "</td>";
+            for (var i in item.condition){
+              line += "<td align='center'>" + item.condition[i] + "</td>";
+            }
             line += "</tr>";
           });
           $("#rdall").empty();
           $("#rdall").append(line);
         });
       }
-
-      // function renderTable(data) {
-      //   var empcard = $("#emp-card");
-      //   empcard.empty();
-
-      //   $.each(data, function(index, value) {
-      //     empcard.append(
-      //       '<tr>' +
-      //       '<th>' + value.license + '</th>' +
-      //       '<td>' + value.name + '</td>' +
-      //       '<td>' + value.position + '</td>' +
-      //       '</tr>')
-
-      //       // '<div class="col-md-6">' +
-      //       // '<div class="card">' +
-      //       // '<div class="card-horizontal">' + 
-      //       // '<div class="card-body">' +
-      //       // '<h5 class="card-title">'+ value.name +'</h5>' +
-      //       // '<p class="card-text">' + value.position +
-      //       // + '</div>'
-      //       // + '</div>'
-      //       // + '</div>'
-      //       // + '</div>')
-      //   });
-      // }
-      // function loadData() {
-      //   var url = "http://localhost/MDTI/index.php/jsonemp";
-      //   $.getJSON(url).done(function (data) {
-      //     console.log(JSON.stringify(data));
-      //     var line = "";
-      //     $.each(data, function (k, item) {
-      //       // console.log(item);
-
-      //       line += "<tr><td align='center'>" + item.license + "</td>";
-      //       line += "<td align='center'>" + item.name + "</td>";
-      //       line += "<td align='center'>" + item.position + "</td>";
-      //       line += "</tr>";
-
-      //       // line += "<div class='col-md-6'>"
-      //       // line += "<div class='card'>"
-      //       // line += "<div class='card-horizontal'>"
-      //       // line += "<h5 class='card-title'>" + item.name + "</h5>"
-      //       // line += "<p class='card-text'>" + item.position + "</p>"
-      //       // line += "</div></div></div></div>";
-      //     });
-      //     $("#emp-card").empty();
-      //     $("#emp-card").append(line);
-      //   });
-      // }
 
       $(function () {
         loadData();
